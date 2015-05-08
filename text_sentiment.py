0a83d79b655b92fbda702e9c37d5795402279fb6
James Nylen <jnylen@gmail.com>
2015-05-08 11:37:27 -0500

    Accept filename(s) on the command line

--- text_sentiment.py
+++ text_sentiment.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import math
 from string import punctuation
@@ -20,22 +21,28 @@ pos_words = pos_words.split("\n")
 neg_words = open('negative.txt').read()
 neg_words = neg_words.split("\n")
 
-words_used = open("analysethis.rtf").read()
+if len(sys.argv) < 2:
+	print "Usage: python text_sentiment.py document.txt"
+	sys.exit(1)
 
-words_used = words_used.lower()
+for filename in sys.argv[1:]:
+	words_used = open(filename).read()
+	words_used = words_used.lower()
 
-for p in list(punctuation):
-	words_used = words_used.replace(p,'')
+	for p in list(punctuation):
+		words_used = words_used.replace(p,'')
 
-for word in words_used.split():
-	if word in pos_words:
-		positive_counter = positive_counter+1
-		total_words = total_words+1
-	elif word in neg_words:
-		negative_counter = negative_counter+1
-		total_words = total_words+1
-	else:
-		total_words = total_words+1
+	for word in words_used.split():
+		if word in pos_words:
+			positive_counter = positive_counter+1
+			total_words = total_words+1
+		elif word in neg_words:
+			negative_counter = negative_counter+1
+			total_words = total_words+1
+		else:
+			total_words = total_words+1
 
-print("Positive Count is " + str(positive_counter) + ", Negative Count is " + str(negative_counter) + ", and the total wordcount is " + str(total_words))
-print("The Ouderkirk Sentiment Score for this text is: " + str(total_words/(positive_counter - negative_counter)))
+	print("")
+	print("File: " + filename)
+	print("Positive Count is " + str(positive_counter) + ", Negative Count is " + str(negative_counter) + ", and the total wordcount is " + str(total_words))
+	print("The Ouderkirk Sentiment Score for this text is: " + str(total_words/(positive_counter - negative_counter)))
